f8fae45f9510361b1b294193cc182982f43b1962
LaciumLux <natalishvecz@mail.ru>
2024-07-08 20:44:53 +0300

    first commit

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,21 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+        userService.saveUser("Ivan", "Ivanov", (byte) 28);
+        userService.saveUser("Petr", "Petrovich", (byte) 18);
+        userService.saveUser("Oleg", "Olegovich", (byte) 35);
+        userService.saveUser("Igor", "Igorevich", (byte) 41);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,27 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/database";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Установка соединения: Complete!" );
+        } catch (SQLException e) {
+            System.err.println("Не удалось установить соединение.");
+        }
+        return connection;
+    }
+
+}
